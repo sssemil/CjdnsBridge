b6809a723554791e9a824db0eccfe5f5bc457fb0
Emil Suleymanov <emil.suleymanov@tum.de>
2019-03-18 09:36:58 +0100

    Kotlinify InternetChecksum

--- src/main/kotlin/sssemil/com/bridge/util/InternetChecksum.kt
+++ src/main/kotlin/sssemil/com/bridge/util/InternetChecksum.kt
@@ -14,61 +14,63 @@
  * limitations under the License.
  */
 
-package sssemil.com.bridge.util;
+package sssemil.com.bridge.util
 
-public class InternetChecksum {
+object InternetChecksum {
 
     /**
      * Calculate the Internet Checksum of a buffer (RFC 1071 - http://www.faqs.org/rfcs/rfc1071.html)
      * Algorithm is
      * 1) apply a 16-bit 1's complement sum over all octets (adjacent 8-bit pairs [A,B], final odd length is [A,0])
      * 2) apply 1's complement to this final sum
-     * <p>
+     *
+     *
      * Notes:
      * 1's complement is bitwise NOT of positive value.
      * Ensure that any carry bits are added back to avoid off-by-one errors
-     * <p>
+     *
+     *
      * From https://stackoverflow.com/a/4114507/3119031.
      *
      * @param buf The message
      * @return The checksum
      */
-    public static long calculateChecksum(byte[] buf) {
-        int length = buf.length;
-        int i = 0;
+    fun calculateChecksum(buf: ByteArray): Long {
+        var length = buf.size
+        var i = 0
 
-        long sum = 0;
-        long data;
+        var sum: Long = 0
+        var data: Long
 
         // Handle all pairs
         while (length > 1) {
             // Corrected to include @Andy's edits and various comments on Stack Overflow
-            data = (((buf[i] << 8) & 0xFF00) | ((buf[i + 1]) & 0xFF));
-            sum += data;
+            data = (buf[i].toInt() shl 8 and 0xFF00 or (buf[i + 1].toInt() and 0xFF)).toLong()
+            sum += data
             // 1's complement carry bit correction in 16-bits (detecting sign extension)
-            if ((sum & 0xFFFF0000) > 0) {
-                sum = sum & 0xFFFF;
-                sum += 1;
+            if (sum and -0x10000 > 0) {
+                sum = sum and 0xFFFF
+                sum += 1
             }
 
-            i += 2;
-            length -= 2;
+            i += 2
+            length -= 2
         }
 
         // Handle remaining byte in odd length buffers
         if (length > 0) {
             // Corrected to include @Andy's edits and various comments on Stack Overflow
-            sum += (buf[i] << 8 & 0xFF00);
+            sum += (buf[i].toInt() shl 8 and 0xFF00).toLong()
             // 1's complement carry bit correction in 16-bits (detecting sign extension)
-            if ((sum & 0xFFFF0000) > 0) {
-                sum = sum & 0xFFFF;
-                sum += 1;
+            if (sum and -0x10000 > 0) {
+                sum = sum and 0xFFFF
+                sum += 1
             }
         }
 
         // Final 1's complement value correction to 16-bits
-        sum = ~sum;
-        sum = sum & 0xFFFF;
-        return sum;
+        sum = sum.inv()
+        sum = sum and 0xFFFF
+        return sum
     }
 }
